f3d26147d2267fcc16200e49084a569e5567e6a4
dogubo mamuzo joshua <emason.tech@gmail.com>
2020-07-03 12:22:17 +0100

    updating

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,49 +1,19 @@
-<<<<<<< HEAD
-require("dotenv").config();
-const Response = require('../util/response_manager'),
-=======
- const Response = require('../util/response_manager'),
-
->>>>>>> 67f6a57971459773016eb5ee87c5294ba0416fc3
-            HttpStatus = require('../util/http_status'),
-            express = require('express'),
-            nodemailer = require('nodemailer'),
-            Customer = require("../models/customer");
-<<<<<<< HEAD
-
-
-const router = express.Router();
-const { MAIL_USERNAME, MAIL_PASSWORD, MAIL_SERVICE } = process.env;
-=======
-            const mailGun = require('nodemailer-mailgun-transport');
-            const router = express.Router();
-            var config = process.env;
->>>>>>> 67f6a57971459773016eb5ee87c5294ba0416fc3
-
-router.use(require("body-parser").urlencoded({extended: true}));
-
-module.exports = {
-    sendMail(req, res){
-<<<<<<< HEAD
-        const transporter = nodemailer.createTransport({
-            service: MAIL_SERVICE,
-            auth: {
+         require("dotenv").config(), 
+          Response = require('../util/response_manager'),
+          HttpStatus = require('../util/http_status'),
+          express = require('express'),
+          nodemailer = require('nodemailer'),Customer = require("../models/customer");
+           const router = express.Router();
+          const { MAIL_USERNAME, MAIL_PASSWORD, MAIL_SERVICE } = process.env;router.use(require("body-parser").urlencoded({extended: true}));
+         module.exports = {
+         sendMail(req, res){const transporter = nodemailer.createTransport({service: MAIL_SERVICE,
+                 auth: {
                 user: MAIL_USERNAME,
-                pass: MAIL_PASSWORD
-=======
-
-            const auth = {
-            auth: {
-                api_key:config.API_KEY,//'17726828da711eda89dcdcc3e664a678-468bde97-fab6abad',
-                domain:config.DOMAIN//'sandbox0a73b939b71e45058d32906c13c2b7e4.mailgun.org'
->>>>>>> 67f6a57971459773016eb5ee87c5294ba0416fc3
+                pass: MAIL_PASSWOR
             }
-        };
-        
-  
-        const transporter = nodemailer.createTransport(mailGun(auth))
-          //Find a customer and get the email
-        let id = req.params.customer_id;
+        });
+        //Find a customer and get the email
+        const id = req.params.customer_id;
         Customer.findById(id, (err, foundCustomer)=>{
             if(err){
                 res.status(404).json({
@@ -53,43 +23,15 @@ module.exports = {
                         statusCode: 404,
                         description: "Could not find a customer with the id: " + id
                     }
-                });
-            }else{
-<<<<<<< HEAD
+                });}else{
                 if(foundCustomer.email  && foundCustomer.email != "Not set" || undefined){
-                    const recipient = foundCustomer.email,
-                        subject = req.body.subject,
-                        text = req.body.text;
-=======
->>>>>>> 67f6a57971459773016eb5ee87c5294ba0416fc3
-
-                if(!foundCustomer==""||undefined){
-                    let myobj = JSON.stringify(foundCustomer);
-                    let data= JSON.parse(myobj)
-                    const recipient = data.email
-                    if(recipient === null || recipient === undefined){
-                         return res.status(401).json({
-                             message:"customer has no email",
-                             code:401
-                        });
-                    }
-                     let subject = req.body.subject;
-                     let text = req.body.text; 
-                    const params = {
-<<<<<<< HEAD
+                    const recipient = foundCustomer.email;
+                    subject = req.body.subject; text = req.body.text;const params = {
                         from: MAIL_USERNAME,
                         to: recipient,
                         subject: subject,
-                        text: text
-=======
-                        from: "example@gmail.com",
-                        to: recipient,
-                        subject: subject,
-                        text:text 
->>>>>>> 67f6a57971459773016eb5ee87c5294ba0416fc3
-                    };
-                    console.log(params);// loginging out params after testing pls delete
-                    transporter.sendMail(params, function (error, result) {
+                        text: text };
+                    transporter.sendMail(params, function (error, info) {
                         if (error) {
                             res.status(401).json({
                                 success: false,
@@ -99,21 +41,13 @@ module.exports = {
                                     description: error
                                 }
                             })
-                        } else {
-<<<<<<< HEAD
-                            res.status(200).json({
+                        }else {
+                        res.status(200).json({
                                 success: true,
                                 message: "Email sent successfully",
                                 data: {
                                     statusCode: 200,
-                                    description: info
-                                }
-=======
-                            console.log('Email sent: ' + result.message);
-                            res.status(200).json({
-                                status: "success",
-                                message: result
->>>>>>> 67f6a57971459773016eb5ee87c5294ba0416fc3
+                                    description: info  }
                             })
                         }
                     });
--- routes/sendMail.js
+++ routes/sendMail.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const emailController = require("./../controllers/emailController");
 const auth = require('../auth/auth');
 
-router.post('/reminder/email/:customer_id', auth, auth,emailController.sendMail);
+router.post('/reminder/email/:customer_id',auth,emailController.sendMail);
 
 module.exports = router
